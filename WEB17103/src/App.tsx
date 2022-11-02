90595a0394643e10a35a00b59ae3b9d472dec743
tuanndph16237 <tuanndph16237@example.com>
2022-11-02 22:24:01 +0700

    Day02

--- WEB17103/src/App.tsx
+++ WEB17103/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Hello from './component/Hello/Hello'
 import Product from './component/Product/Product'
+import Lifecycle from './Lifecycle/Lifecycle'
 function App() {
   const [count, setCount] = useState(0)
   function hello(props: {name : string }): string{
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Product/>
       <Hello name="Nguyen Duc Tuan" age={20}/>
+      <Lifecycle/>
     </div>
   )
 }
